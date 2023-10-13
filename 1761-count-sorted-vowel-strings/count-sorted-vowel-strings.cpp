06d907d9a21c21dd7015e59c70c95d1fd13deb5f
madison-han <104019938+madison-han@users.noreply.github.com>
2023-10-12 23:54:31 -0400

    Time: 0 ms (100.00%) | Memory: 6.3 MB (49.15%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        if (n == 0) return 0;
-        else if (n == 1) return 5;
-
-
         return ((n+1)*(n+2)*(n+3)*(n+4))/24;
 
     }
