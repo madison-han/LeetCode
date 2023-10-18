6462047caf4ea0cb4beb700d60c29bb740ff44c8
madison-han <104019938+madison-han@users.noreply.github.com>
2023-10-17 22:01:37 -0400

    Time: 20 ms (12.02%) | Memory: 19.9 MB (31.54%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,20 +5,21 @@ public:
 
         if (n <= 2) return nums[0];
 
-        map<int, int> count;
-
-        int temp = 0;
+        int major = nums[0], count = 1;
 
 
-        for (int i = 0; i < n; i++) {
-            temp = nums[i]++;
-            count[temp]++;
-
-            if (count[temp] > (n / 2)) return temp;
-
+        for (int i = 1; i < n; i++) {
+            if (count == 0) {
+                count++;
+                major = nums[i];
+            } else if (major == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
         }
 
-        return 0;
+        return major;
         
     }
 };
