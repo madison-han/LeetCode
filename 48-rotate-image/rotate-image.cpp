864c80c3c30925cf0545035c78db84920bdda7e1
madison-han <104019938+madison-han@users.noreply.github.com>
2024-07-12 22:08:57 -0400

    Time: 0 ms (100.00%) | Memory: 9 MB (23.00%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        if (n <= 1) return;
+
+        // Flip across x-axis
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-i][j];
+                matrix[n-1-i][j] = temp;
+            }
+        }
+
+        // Flip across y=x
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+
+
+            
+        
+    }
+
+};
