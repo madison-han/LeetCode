1ddd2799897a4efacf3d36419783204934d80928
madison-han <104019938+madison-han@users.noreply.github.com>
2025-03-01 01:33:59 -0500

    Time: 3 ms (36.59%) | Memory: 11.4 MB (66.14%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.cpp
+++ 926-find-and-replace-pattern/find-and-replace-pattern.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string> ans;
+        int len = pattern.size();
 
         for (string word : words) {
-            if (word.length() != pattern.length()) continue;  
             
             unordered_map<char, char> pToW;  
             unordered_map<char, char> wToP; 
             
             bool valid = true;
-            for (int i = 0; i < word.length(); i++) {
+            for (int i = 0; i < len; i++) {
                 char p = pattern[i], w = word[i];
 
                 if (pToW.count(p) && pToW[p] != w) {
