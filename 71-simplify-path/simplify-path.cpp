f886c725f416fd63ee6db4b0e06d315119210971
madison-han <104019938+madison-han@users.noreply.github.com>
2025-03-26 23:59:06 -0400

    Time: 0 ms (100.00%) | Memory: 10.6 MB (78.82%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        if (path.length() == 1) return "/";
-        string ans = "";
-
-        stack<string> st;
-
-        int n = path.length();
-        string temp="";
-
-        for (int i =0; i< n; i++) {
-            if (path[i] =='/') continue;
-
-            temp = "";
-            while (i < n && path[i] != '/') {
-                temp += path[i++];
-            }
-
-            if (temp == ".") continue;
-            else if (temp == "..") {
-                if (!st.empty()) st.pop();
-            } else {
-                st.push(temp);
-            }
-        }
-
-        while(!st.empty()) {
-            ans = "/" + st.top() + ans;
-            st.pop();
-        }
-
-        if (ans == "") {ans = "/";}
-        return ans;
+        vector<string> components;
+        stringstream ss(path);
+        string component;
         
+        while (getline(ss, component, '/')) {
+            if (component.empty() || component == ".") continue;
+            
+            if (component == "..") {
+                if (!components.empty()) {
+                    components.pop_back();
+                }
+            } else {
+                components.push_back(component);
+            }
+        }
+        
+        if (components.empty()) return "/";
+        
+        string simplifiedPath;
+        for (const string& dir : components) {
+            simplifiedPath += "/" + dir;
+        }
+        
+        return simplifiedPath;
     }
 };
