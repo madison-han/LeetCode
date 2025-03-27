3349a491af063f16f846c3d16aaf5f6c2593360b
madison-han <104019938+madison-han@users.noreply.github.com>
2025-03-26 21:15:13 -0400

    Time: 3 ms (42.93%) | Memory: 9.3 MB (54.76%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         int n = s.length();
-        if (n==1) return true;
+        if (n == 1) return true;
 
         unordered_map<char, char> map;
-        unordered_map<char, bool>alph_vals;
+        unordered_map<char, bool> usedVals;
 
-        for (int i = 0; i <n; i++) {
-            if (map.find(s[i]) != map.end() || alph_vals[t[i]-97]) {
-                if (map[s[i]] != t[i]){
+        for (int i = 0; i < n; i++) {
+            if (map.find(s[i]) != map.end()) {
+                if (map[s[i]] != t[i]) {
                     return false;
                 }
             } else {
+                if (usedVals[t[i]]) {
+                    return false;
+                }
                 map[s[i]] = t[i];
-                alph_vals[t[i]-97]= true;
+                usedVals[t[i]] = true;
             }
         }
 
-        return true;        
+        return true;              
     }
 };
